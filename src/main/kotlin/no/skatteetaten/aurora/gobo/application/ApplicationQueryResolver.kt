842fc46d5eb5be6c4ddf950719edbb3df3194cc7
Jarle Hansen <jarle.hansen@skatteetaten.no>
2018-05-29 15:29:43 +0200

    added the option to send in a list of affiliations when getting applications

--- src/main/kotlin/no/skatteetaten/aurora/gobo/application/ApplicationQueryResolver.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/application/ApplicationQueryResolver.kt
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
+import org.springframework.web.util.UriComponentsBuilder
 
 @Component
 class ApplicationQueryResolver(
@@ -15,11 +17,18 @@ class ApplicationQueryResolver(
     val objectMapper: ObjectMapper
 ) : GraphQLQueryResolver {
 
-    fun getApplications(affiliation: String): List<Application> {
-        val response =
-            restTemplate.getForObject<String>("$mokeyUrl/api/application?affiliation=$affiliation")
-                ?: return emptyList()
-
+    fun getApplications(affiliations: List<String>): List<Application> {
+        val response = restTemplate.getForObject<String>(getUrl(affiliations)) ?: return emptyList()
         return objectMapper.readValue(response)
+    }
+
+    private fun getUrl(affiliations: List<String>): String {
+        val parameters = LinkedMultiValueMap<String, String>().apply {
+            addAll("affiliation", affiliations)
+        }
+        val builder = UriComponentsBuilder
+            .fromUriString("$mokeyUrl/api/application")
+            .queryParams(parameters)
+        return builder.toUriString()
     }
 }
