6eb14df986bf46384f16adf9341d5b02641d8bce
Jarle Hansen <jarle.hansen@skatteetaten.no>
2018-06-06 15:10:20 +0200

    added hasPreviousPage to PageInfo

--- src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/PageInfo.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/PageInfo.kt
@@ -1,3 +1,3 @@
 package no.skatteetaten.aurora.gobo.resolvers
 
-data class PageInfo(val startCursor: String?, val endCursor: String?, val hasNextPage: Boolean?)
+data class PageInfo(val startCursor: String?, val endCursor: String?, val hasPreviousPage: Boolean, val hasNextPage: Boolean?)
