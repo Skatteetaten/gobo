1b392bfb9870d2a40eb2a383f4fab55647963a11
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2018-08-10 12:10:24 +0200

    Simplifications and tests

--- src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/paging.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/paging.kt
@@ -3,10 +3,11 @@ package no.skatteetaten.aurora.gobo.resolvers
 import graphql.relay.DefaultEdge
 import graphql.relay.DefaultPageInfo
 import graphql.relay.PageInfo
+import java.lang.Math.min
 
 data class PagedEdges<T>(val edges: List<T>, val pageInfo: PageInfo, val totalCount: Int)
 
-fun <T : DefaultEdge<*>> pageEdges(allEdges: List<T>, first: Int?, after: String?): PagedEdges<T> {
+fun <T : DefaultEdge<*>> pageEdges(allEdges: List<T>, first: Int? = null, after: String? = null): PagedEdges<T> {
 
     val edges = createPage(allEdges, first, after)
     val pageInfo = createPageInfo(edges, allEdges)
@@ -15,8 +16,8 @@ fun <T : DefaultEdge<*>> pageEdges(allEdges: List<T>, first: Int?, after: String
 
 private fun <T : DefaultEdge<*>> createPage(edges: List<T>, first: Int?, after: String?): List<T> {
 
-    val startIndex = if (after != null) edges.indexOfOrNull { it.cursor.value == after } ?: 0 else 0
-    val endIndex = if (first != null) startIndex + first else edges.size
+    val startIndex = if (after != null) edges.indexOfFirst { it.cursor.value == after } + 1 else 0
+    val endIndex = if (first != null) min(startIndex + first, edges.size) else edges.size
 
     return edges.subList(startIndex, endIndex)
 }
@@ -35,6 +36,3 @@ private fun <T : DefaultEdge<*>> createPageInfo(pageEdges: List<T>, allEdges: Li
     val all = Cursors(allEdges)
     return DefaultPageInfo(page.first, page.last, page.isAtStartOf(all), page.isAtEndOf(all))
 }
-
-private fun <E> List<E>.indexOfOrNull(function: (it: E) -> Boolean): Int? =
-    this.indexOfFirst { function(it) }.takeIf { it != -1 }
