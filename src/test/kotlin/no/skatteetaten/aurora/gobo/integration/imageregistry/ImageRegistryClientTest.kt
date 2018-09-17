d76210b7f61be43b06cc76e81b9816fdd47c8790
Bjarte Stien Karlsen <bjarte.karlsen@skatteetate.no>
2018-09-17 09:40:35 +0200

    added test for tags

--- src/test/kotlin/no/skatteetaten/aurora/gobo/integration/imageregistry/ImageRegistryClientTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/gobo/integration/imageregistry/ImageRegistryClientTest.kt
@@ -8,10 +8,12 @@ import com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import com.github.tomakehurst.wiremock.client.WireMock.get
 import com.github.tomakehurst.wiremock.client.WireMock.matching
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.verify
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
 import ru.lanwen.wiremock.ext.WiremockResolver
 import ru.lanwen.wiremock.ext.WiremockResolver.Wiremock
@@ -24,6 +26,39 @@ class ImageRegistryClientTest {
     val tokenProvider = mockk<TokenProvider>()
 
     val service = ImageRegistryClient(WebClient.create(), tokenProvider)
+
+    private val tagsListResponse = """{
+   "name": "no_skatteetaten_aurora/boober",
+   "tags": [
+     "master-SNAPSHOT",
+     "1.0.0-rc.1-b2.2.3-oracle8-1.4.0",
+     "1.0.0-rc.2-b2.2.3-oracle8-1.4.0",
+     "develop-SNAPSHOT",
+     "1"
+   ]
+ }"""
+
+    @Test
+    fun `fetch taglist`(@Wiremock server: WireMockServer, @WiremockUri uri: String) {
+
+        every { tokenProvider.token } returns "token"
+
+        server.stubFor(
+            get(urlEqualTo("/taglist"))
+                .willReturn(
+                    aResponse()
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+
+                        .withBody(tagsListResponse)
+                )
+        )
+        val manifestUri = "$uri/taglist"
+        val tags = service.getTags(manifestUri, AuthenticationMethod.KUBERNETES_TOKEN)
+        assert(tags?.name).isEqualTo("no_skatteetaten_aurora/boober")
+        assert(tags?.tags?.size).isEqualTo(5)
+
+
+    }
 
     @Test
     fun `fetch manifest`(@Wiremock server: WireMockServer, @WiremockUri uri: String) {
