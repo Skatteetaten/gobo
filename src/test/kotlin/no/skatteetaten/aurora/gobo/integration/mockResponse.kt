14ccbd6311d67338a4c987d806276e6e6d0dc975
Jarle Hansen <jarle.hansen@skatteetaten.no>
2018-09-17 13:33:31 +0200

    specify return type MockResponse

--- src/test/kotlin/no/skatteetaten/aurora/gobo/integration/mockResponse.kt
+++ src/test/kotlin/no/skatteetaten/aurora/gobo/integration/mockResponse.kt
@@ -4,7 +4,7 @@ import okhttp3.mockwebserver.MockResponse
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 
-fun createJsonMockResponse(status: Int = 200, body: String = "") =
+fun createJsonMockResponse(status: Int = 200, body: String = ""): MockResponse =
         MockResponse()
                 .setResponseCode(status)
                 .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE)
