b11565e46ed5395d71364f3e95fc49520245384d
Jarle Hansen <jarle.hansen@skatteetaten.no>
2018-09-25 08:23:17 +0200

    added test for refreshApplicationDeployment mutation

--- src/test/kotlin/no/skatteetaten/aurora/gobo/resolvers/applicationdeployment/ApplicationDeploymentMutationResolverTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/gobo/resolvers/applicationdeployment/ApplicationDeploymentMutationResolverTest.kt
@@ -4,6 +4,7 @@ import no.skatteetaten.aurora.gobo.GraphQLTest
 import no.skatteetaten.aurora.gobo.resolvers.createQuery
 import no.skatteetaten.aurora.gobo.service.ApplicationUpgradeService
 import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.mock.mockito.MockBean
@@ -15,6 +16,9 @@ import org.springframework.web.reactive.function.BodyInserters
 class ApplicationDeploymentMutationResolverTest {
     @Value("classpath:graphql/redeployWithVersion.graphql")
     private lateinit var redeployWithVersionMutation: Resource
+
+    @Value("classpath:graphql/refreshApplicationDeployment.graphql")
+    private lateinit var refreshApplicationDeploymentMutation: Resource
 
     @Autowired
     private lateinit var webTestClient: WebTestClient
@@ -39,6 +43,26 @@ class ApplicationDeploymentMutationResolverTest {
             .exchange()
             .expectStatus().isOk
             .expectBody()
-            .jsonPath("$.data.redeployWithVersion").isBoolean
+            .jsonPath("$.data.redeployWithVersion").isNotEmpty
+    }
+
+    @Test
+    fun `Mutate refresh application deployment`() {
+        given(applicationUpgradeService.refreshApplicationDeployment("123")).willReturn("123")
+
+        val variables = mapOf(
+            "input" to mapOf(
+                "applicationDeploymentId" to "123"
+            )
+        )
+        val query = createQuery(refreshApplicationDeploymentMutation, variables)
+        webTestClient
+            .post()
+            .uri("/graphql")
+            .body(BodyInserters.fromObject(query))
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("$.data.refreshApplicationDeployment").isNotEmpty
     }
 }
