de2a448c69d993a2f2014624a6f2433ebf2c6d0c
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2018-10-04 14:42:19 +0200

    Getting token from the DataFetchingEnvironment instead of the Securit… (#27)
    * Getting token from the DataFetchingEnvironment instead of the SecurityContextHolder.
    
    * Decomposition

--- src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/applicationdeploymentdetails/DeploymentSpecDataLoader.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/applicationdeploymentdetails/DeploymentSpecDataLoader.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import no.skatteetaten.aurora.gobo.integration.boober.AuroraConfigService
 import no.skatteetaten.aurora.gobo.resolvers.KeysDataLoader
-import no.skatteetaten.aurora.gobo.security.UserService
 import no.skatteetaten.aurora.utils.logLine
 import no.skatteetaten.aurora.utils.time
 import org.dataloader.Try
@@ -15,24 +14,25 @@ import org.springframework.util.StopWatch
 import reactor.core.publisher.toMono
 import java.net.URL
 
+data class UrlAndToken(val url: URL, val token: String)
+
 @Component
 class DeploymentSpecDataLoader(
-    private val userService: UserService,
     private val configService: AuroraConfigService,
     private val objectMapper: ObjectMapper
-) : KeysDataLoader<URL, Try<DeploymentSpec>> {
+) : KeysDataLoader<UrlAndToken, Try<DeploymentSpec>> {
 
     private val logger: Logger = LoggerFactory.getLogger(DeploymentSpecDataLoader::class.java)
 
-    override fun getByKeys(keys: List<URL>): List<Try<DeploymentSpec>> {
+    override fun getByKeys(keys: List<UrlAndToken>): List<Try<DeploymentSpec>> {
 
         logger.debug("Loading ${keys.size} DeploymentSpecs from boober (${keys.toSet().size} unique)")
 
         val sw = StopWatch()
         val specs: List<Try<DeploymentSpec>> = sw.time("Fetch ${keys.size} DeploymentSpecs") {
-            keys.map { url ->
+            keys.map { (url, token) ->
                 Try.tryCall {
-                    configService.get<JsonNode>(userService.getToken(), url.toString())
+                    configService.get<JsonNode>(token, url.toString())
                         .toMono()
                         .map { DeploymentSpec(jsonRepresentation = objectMapper.writeValueAsString(it)) }
                         .block() ?: throw IllegalArgumentException("Empty DeploymentSpec with url=$url")
--- src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/applicationdeploymentdetails/DeploymentSpecsResolver.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/applicationdeploymentdetails/DeploymentSpecsResolver.kt
@@ -1,20 +1,24 @@
 package no.skatteetaten.aurora.gobo.resolvers.applicationdeploymentdetails
 
 import com.coxautodev.graphql.tools.GraphQLResolver
+import graphql.schema.DataFetchingEnvironment
 import no.skatteetaten.aurora.gobo.resolvers.NoCacheBatchDataLoader
+import no.skatteetaten.aurora.gobo.resolvers.token
 import org.dataloader.Try
 import org.springframework.stereotype.Component
-import java.net.URL
 import java.util.concurrent.CompletableFuture
 
 @Component
-class DeploymentSpecsResolver(private val specLoader: NoCacheBatchDataLoader<URL, Try<DeploymentSpec>>) : GraphQLResolver<DeploymentSpecs> {
+class DeploymentSpecsResolver(private val specLoader: NoCacheBatchDataLoader<UrlAndToken, Try<DeploymentSpec>>) :
+    GraphQLResolver<DeploymentSpecs> {
 
-    fun current(deploymentSpecs: DeploymentSpecs): CompletableFuture<Try<DeploymentSpec>>? {
-        return deploymentSpecs.deploymentSpecCurrent?.let { specLoader.load(it) }
-    }
+    fun current(specs: DeploymentSpecs, dfe: DataFetchingEnvironment): CompletableFuture<Try<DeploymentSpec>>? =
+        dfe.token?.let { token ->
+            specs.deploymentSpecCurrent?.let { specLoader.load(UrlAndToken(it, token)) }
+        }
 
-    fun deployed(deploymentSpecs: DeploymentSpecs): CompletableFuture<Try<DeploymentSpec>>? {
-        return deploymentSpecs.deploymentSpecDeployed?.let { specLoader.load(it) }
-    }
+    fun deployed(specs: DeploymentSpecs, dfe: DataFetchingEnvironment): CompletableFuture<Try<DeploymentSpec>>? =
+        dfe.token?.let { token ->
+            specs.deploymentSpecDeployed?.let { specLoader.load(UrlAndToken(it, token)) }
+        }
 }
