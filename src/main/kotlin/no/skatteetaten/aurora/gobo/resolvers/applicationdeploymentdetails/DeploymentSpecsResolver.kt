c3e07a359171a6aa765582eab8d9f6ef35ea5fcf
Jarle Hansen <jarle.hansen@skatteetaten.no>
2018-10-12 10:49:05 +0200

    get token from userService instead of DataFetchingEnvironment

--- src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/applicationdeploymentdetails/DeploymentSpecsResolver.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/applicationdeploymentdetails/DeploymentSpecsResolver.kt
@@ -3,21 +3,21 @@ package no.skatteetaten.aurora.gobo.resolvers.applicationdeploymentdetails
 import com.coxautodev.graphql.tools.GraphQLResolver
 import graphql.schema.DataFetchingEnvironment
 import no.skatteetaten.aurora.gobo.resolvers.loader
-import no.skatteetaten.aurora.gobo.resolvers.token
+import no.skatteetaten.aurora.gobo.security.UserService
 import org.dataloader.Try
 import org.springframework.stereotype.Component
 import java.util.concurrent.CompletableFuture
 
 @Component
-class DeploymentSpecsResolver : GraphQLResolver<DeploymentSpecs> {
+class DeploymentSpecsResolver(private val userService: UserService) : GraphQLResolver<DeploymentSpecs> {
 
     fun current(specs: DeploymentSpecs, dfe: DataFetchingEnvironment): CompletableFuture<Try<DeploymentSpec>>? =
-        dfe.token?.let { token ->
+        userService.getToken().let { token ->
             specs.deploymentSpecCurrent?.let { dfe.loader(DeploymentSpec::class).load(UrlAndToken(it, token)) }
         }
 
     fun deployed(specs: DeploymentSpecs, dfe: DataFetchingEnvironment): CompletableFuture<Try<DeploymentSpec>>? =
-        dfe.token?.let { token ->
+        userService.getToken().let { token ->
             specs.deploymentSpecDeployed?.let { dfe.loader(DeploymentSpec::class).load(UrlAndToken(it, token)) }
         }
 }
