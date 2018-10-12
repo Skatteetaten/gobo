cdc404a3cd42bdaa15fbf1c021e7c14ef2d31c79
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2018-10-12 10:05:02 +0200

    use filter and not auth.

--- src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/affiliation/AffiliationQueryResolver.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/affiliation/AffiliationQueryResolver.kt
@@ -3,7 +3,6 @@ package no.skatteetaten.aurora.gobo.resolvers.affiliation
 import com.coxautodev.graphql.tools.GraphQLQueryResolver
 import no.skatteetaten.aurora.gobo.integration.mokey.AffiliationService
 import no.skatteetaten.aurora.gobo.security.UserService
-import no.skatteetaten.aurora.gobo.security.UserService.Companion.GUEST_USER_NAME
 import org.springframework.stereotype.Component
 
 @Component
@@ -12,11 +11,11 @@ class AffiliationQueryResolver(
     val userService: UserService
 ) : GraphQLQueryResolver {
 
-    fun getAffiliations(): AffiliationsConnection {
-        val affiliationNames = if (userService.getCurrentUser().name == GUEST_USER_NAME) {
-            affiliationService.getAllAffiliations()
-        } else {
+    fun getAffiliations(checkForVisibility: Boolean = false): AffiliationsConnection {
+        val affiliationNames = if (checkForVisibility) {
             affiliationService.getAllVisibleAffiliations(userService.getToken())
+        } else {
+            affiliationService.getAllAffiliations()
         }
         val edges = affiliationNames.map { AffiliationEdge(Affiliation(it)) }
         return AffiliationsConnection(edges, null)
