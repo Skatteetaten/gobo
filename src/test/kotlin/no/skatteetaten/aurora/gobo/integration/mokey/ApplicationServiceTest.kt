dde2949537ddba49fac6d0b2953c931e660ddec0
Jarle Hansen <jarle@jarlehansen.net>
2019-01-04 12:23:39 +0100

    Fix contract tests (#53)
    * updated contract test config and fixed disabled tests
    
    * added logging to view the resolved artifact from the stubrunner

--- src/test/kotlin/no/skatteetaten/aurora/gobo/integration/mokey/ApplicationServiceTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/gobo/integration/mokey/ApplicationServiceTest.kt
@@ -3,7 +3,6 @@ package no.skatteetaten.aurora.gobo.integration.mokey
 import assertk.assert
 import assertk.assertions.isNotEmpty
 import assertk.assertions.isNotNull
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -20,13 +19,11 @@ class ApplicationServiceTest {
     lateinit var applicationService: ApplicationServiceBlocking
 
     @Test
-    @Disabled("need to fix contract tests in mokey")
     fun `Get applications for affiliation`() {
         val applications = applicationService.getApplications(listOf("paas"))
         assert(applications).isNotEmpty()
     }
 
-    @Disabled("change to fetch for one id")
     @Test
     fun `Get application deployment details for affiliation`() {
         val details = applicationService.getApplicationDeploymentDetails("paas", "foo")
