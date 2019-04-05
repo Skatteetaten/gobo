6dbe5b65ed584c79efe244f84a66b2adf9ad2a76
Jarle Hansen <jarle@jarlehansen.net>
2019-04-05 12:34:48 +0200

    log source system (#97)

--- src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/errorhandling/GoboDataFetcherExceptionHandler.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/errorhandling/GoboDataFetcherExceptionHandler.kt
@@ -4,6 +4,7 @@ import graphql.execution.DataFetcherExceptionHandler
 import graphql.execution.DataFetcherExceptionHandlerParameters
 import mu.KotlinLogging
 import no.skatteetaten.aurora.gobo.GoboException
+import no.skatteetaten.aurora.gobo.integration.SourceSystemException
 
 private val logger = KotlinLogging.logger { }
 
@@ -15,6 +16,13 @@ class GoboDataFetcherExceptionHandler : DataFetcherExceptionHandler {
             handlerParameters.executionContext?.addError(GraphQLExceptionWrapper(handlerParameters))
         }
 
-        logger.error { "Exception in data fetcher: ${handlerParameters.exception.message}" }
+        val exceptionMessage = if (handlerParameters.exception is SourceSystemException) {
+            val exception = handlerParameters.exception as SourceSystemException
+            "Exception in data fetcher, exception=\"${exception.message}\" - source=${exception.sourceSystem}"
+        } else {
+            "Exception in data fetcher, exception=\"${handlerParameters.exception.message}\""
+        }
+
+        logger.error { exceptionMessage }
     }
 }
