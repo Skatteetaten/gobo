12df6da7aefaf763498647cee6e714c003fb822c
Erlend Haugsnes <Erlend.Haugsnes@skatteetaten.no>
2019-08-13 11:53:20 +0200

    version bump. Gradle 5.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.31"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.31"
-    id("org.jlleitschuh.gradle.ktlint") version "8.0.0"
+    id("org.jetbrains.kotlin.jvm") version "1.3.41"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.41"
+    id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
     id("org.sonarqube") version "2.7.1"
 
-    id("org.springframework.boot") version "2.1.5.RELEASE"
-    id("org.asciidoctor.convert") version "2.2.0"
+    id("org.springframework.boot") version "2.1.7.RELEASE"
+    id("org.asciidoctor.convert") version "2.3.0"
 
     id("com.gorylenko.gradle-git-properties") version "2.0.0"
-    id("com.github.ben-manes.versions") version "0.21.0"
-    id("se.patrikerdes.use-latest-versions") version "0.2.9"
+    id("com.github.ben-manes.versions") version "0.22.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.12"
 
-    id("no.skatteetaten.gradle.aurora") version "2.4.0"
+    id("no.skatteetaten.gradle.aurora") version "2.4.2"
 }
 
 val springCloudContractVersion: String = project.property("aurora.springCloudContractVersion") as String
@@ -29,8 +29,8 @@ dependencies {
     implementation("com.graphql-java-kickstart:graphql-java-tools:5.6.0")
 
     implementation("uk.q3c.rest:hal-kotlin:0.5.4.0.db32476")
-    implementation("io.fabric8:openshift-client:4.2.2")
-    implementation("com.fkorotkov:kubernetes-dsl:2.0.1")
+    implementation("io.fabric8:openshift-client:4.4.1")
+    implementation("com.fkorotkov:kubernetes-dsl:2.1")
     implementation("com.github.fge:json-patch:1.9")
     implementation("com.jayway.jsonpath:json-path:2.4.0")
 
