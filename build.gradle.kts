fde078afe34105f51463654440f3430566a5d02b
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2019-09-13 14:51:37 +0200

    updated dependencits

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,21 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.41"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.41"
+    id("org.jetbrains.kotlin.jvm") version "1.3.50"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.50"
     id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
     id("org.sonarqube") version "2.7.1"
 
-    id("org.springframework.boot") version "2.1.7.RELEASE"
+    id("org.springframework.boot") version "2.1.8.RELEASE"
     id("org.asciidoctor.convert") version "2.3.0"
 
-    id("com.gorylenko.gradle-git-properties") version "2.0.0"
-    id("com.github.ben-manes.versions") version "0.22.0"
+    id("com.gorylenko.gradle-git-properties") version "2.1.0"
+    id("com.github.ben-manes.versions") version "0.25.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.12"
     id("no.skatteetaten.gradle.aurora") version "2.4.2"
 }
 
 val springCloudContractVersion: String = project.property("aurora.springCloudContractVersion") as String
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.1")
 
     implementation("org.springframework.retry:spring-retry")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -28,7 +28,7 @@ dependencies {
     implementation("com.graphql-java-kickstart:graphql-java-tools:5.6.1")
 
     implementation("uk.q3c.rest:hal-kotlin:0.5.4.0.db32476")
-    implementation("io.fabric8:openshift-client:4.4.1")
+    implementation("io.fabric8:openshift-client:4.5.1")
     implementation("com.fkorotkov:kubernetes-dsl:2.2")
     implementation("com.github.fge:json-patch:1.9")
     implementation("com.jayway.jsonpath:json-path:2.4.0")
