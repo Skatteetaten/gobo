f6e248b6ed7d805948420c185c9c201876556481
Jarle Hansen <jarle.hansen@skatteetaten.no>
2020-07-14 15:15:27 +0200

    use webflux only

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("com.gorylenko.gradle-git-properties") version "2.2.1"
     id("com.github.ben-manes.versions") version "0.27.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.13"
-    id("no.skatteetaten.gradle.aurora") version "3.4.5"
+    id("no.skatteetaten.gradle.aurora") version "3.6.1"
 }
 
 val springCloudContractVersion: String = project.property("aurora.springCloudContractVersion") as String
