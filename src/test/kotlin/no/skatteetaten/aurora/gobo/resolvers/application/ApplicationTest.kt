f6d68353fef397a8b38c33244b3e47e7c16d37fd
Jarle Hansen <jarle.hansen@skatteetaten.no>
2020-08-24 07:59:04 +0200

    updated dependencies

--- src/test/kotlin/no/skatteetaten/aurora/gobo/resolvers/application/ApplicationTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/gobo/resolvers/application/ApplicationTest.kt
@@ -10,21 +10,18 @@ class ApplicationTest {
     @Test
     fun `verify internal registry is recognized as internal`() {
         val isInternal = DockerRegistryUtil.isInternal("docker-registry.default.svc:5000")
-
         assertThat(isInternal).isTrue()
     }
 
     @Test
     fun `verify central registry is recognized as not internal`() {
         val isInternal = DockerRegistryUtil.isInternal("docker-registry.somesuch.no:5000")
-
         assertThat(isInternal).isFalse()
     }
 
     @Test
     fun `verify internal IP with path is recognized as internal registry`() {
         val isInternal = DockerRegistryUtil.isInternal("127.0.0.1:5000/test123")
-
         assertThat(isInternal).isTrue()
     }
 }
