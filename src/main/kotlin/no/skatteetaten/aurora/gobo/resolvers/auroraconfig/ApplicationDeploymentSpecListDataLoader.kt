0ae2b02187f554ab847213c43f03dcdfdff92972
Jarle Hansen <jarle.hansen@skatteetaten.no>
2020-09-21 15:04:55 +0200

    check for token

--- src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/auroraconfig/ApplicationDeploymentSpecListDataLoader.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/resolvers/auroraconfig/ApplicationDeploymentSpecListDataLoader.kt
@@ -2,6 +2,7 @@ package no.skatteetaten.aurora.gobo.resolvers.auroraconfig
 
 import no.skatteetaten.aurora.gobo.KeyDataLoader
 import no.skatteetaten.aurora.gobo.integration.boober.ApplicationDeploymentService
+import no.skatteetaten.aurora.gobo.resolvers.AccessDeniedException
 import no.skatteetaten.aurora.gobo.resolvers.GoboGraphQLContext
 import no.skatteetaten.aurora.gobo.resolvers.applicationdeployment.ApplicationDeploymentRef
 import org.springframework.stereotype.Component
@@ -14,11 +15,13 @@ class ApplicationDeploymentSpecListDataLoader(
 ) : KeyDataLoader<AdSpecKey, List<ApplicationDeploymentSpec>> {
 
     override suspend fun getByKey(key: AdSpecKey, context: GoboGraphQLContext): List<ApplicationDeploymentSpec> {
-        return applicationDeploymentService.getSpec(
-            context.token!!,
-            key.configName,
-            key.configRef,
-            key.applicationDeploymentRef
-        )
+        return context.token?.let {
+            applicationDeploymentService.getSpec(
+                token = it,
+                auroraConfigName = key.configName,
+                auroraConfigReference = key.configRef,
+                applicationDeploymentReferenceList = key.applicationDeploymentRef
+            )
+        } ?: throw AccessDeniedException("No anonymous access") // TODO error handling
     }
 }
