2cd8afbbae34bde872200cc6f24cdf58340ee1e1
Jarle Hansen <jarle.hansen@skatteetaten.no>
2020-09-23 12:24:04 +0200

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
     id("org.jetbrains.kotlin.plugin.spring") version "1.3.72"
-    id("org.jlleitschuh.gradle.ktlint") version "9.3.0"
     id("org.sonarqube") version "3.0"
 
-    id("org.springframework.boot") version "2.3.3.RELEASE"
+    id("org.springframework.boot") version "2.3.4.RELEASE"
     id("org.asciidoctor.convert") version "2.4.0"
 
     id("com.gorylenko.gradle-git-properties") version "2.2.3"
-    id("com.github.ben-manes.versions") version "0.29.0"
+    id("com.github.ben-manes.versions") version "0.33.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.14"
-    id("no.skatteetaten.gradle.aurora") version "3.6.6"
+    id("no.skatteetaten.gradle.aurora") version "4.0.4"
 }
 
 val springCloudContractVersion: String = project.property("aurora.springCloudContractVersion") as String
@@ -21,10 +20,10 @@ dependencies {
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.0.2.RELEASE")
     implementation("org.apache.commons:commons-text:1.9")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("com.expediagroup:graphql-kotlin-spring-server:3.6.1")
+    implementation("com.expediagroup:graphql-kotlin-spring-server:3.6.4")
 
     implementation("uk.q3c.rest:hal-kotlin:0.5.4.0.db32476")
-    implementation("io.fabric8:openshift-client:4.10.3")
+    implementation("io.fabric8:openshift-client:4.11.1")
     implementation("com.github.fge:json-patch:1.13")
     implementation("com.jayway.jsonpath:json-path:2.4.0")
     implementation("io.projectreactor.addons:reactor-extra:3.3.3.RELEASE")
