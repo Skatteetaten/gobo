9d45b7f3c44e89919cc20b1237d8136b4f5cf491
Jarle Hansen <jarle.hansen@skatteetaten.no>
2020-10-05 09:43:37 +0200

    added configurable thread pool for data loaders

--- src/main/kotlin/no/skatteetaten/aurora/gobo/DataLoaderConfiguration.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/DataLoaderConfiguration.kt
@@ -1,10 +1,10 @@
 package no.skatteetaten.aurora.gobo
 
 import com.expediagroup.graphql.spring.execution.DataLoaderRegistryFactory
-import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.ExecutorCoroutineDispatcher
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.future.asCompletableFuture
 import mu.KotlinLogging
 import no.skatteetaten.aurora.gobo.resolvers.GoboGraphQLContext
@@ -13,8 +13,10 @@ import org.dataloader.DataLoader
 import org.dataloader.DataLoaderOptions
 import org.dataloader.DataLoaderRegistry
 import org.dataloader.Try
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.concurrent.Executors
 
 interface KeyDataLoader<K, V> {
     suspend fun getByKey(key: K, context: GoboGraphQLContext): V
@@ -28,19 +30,21 @@ private val logger = KotlinLogging.logger { }
 
 @Configuration
 class DataLoaderConfiguration(
+    @Value("\${gobo.dataloader.thread-pool-size:4}") val threadPoolSize: Int,
     val keyLoaders: List<KeyDataLoader<*, *>>,
     val multipleKeysDataLoaders: List<MultipleKeysDataLoader<*, *>>
 ) {
     @Bean
     fun dataLoaderRegistryFactory(): DataLoaderRegistryFactory {
+        val coroutineDispatcher = Executors.newFixedThreadPool(threadPoolSize).asCoroutineDispatcher()
         val registry = DataLoaderRegistry().apply {
             keyLoaders.forEach {
                 logger.debug("Registering KeyDataLoader: ${it::class.simpleName}")
-                register(it::class.simpleName, batchDataLoaderMappedSingle(it))
+                register(it::class.simpleName, batchDataLoaderMappedSingle(coroutineDispatcher, it))
             }
             multipleKeysDataLoaders.forEach {
                 logger.debug("Registering MultipleKeysDataLoader: ${it::class.simpleName}")
-                register(it::class.simpleName, batchDataLoaderMappedMultiple(it))
+                register(it::class.simpleName, batchDataLoaderMappedMultiple(coroutineDispatcher, it))
             }
         }
 
@@ -54,10 +58,13 @@ class DataLoaderConfiguration(
     /**
      * Use this if you have a service that loads multiple ids.
      */
-    private fun <K, V> batchDataLoaderMappedMultiple(keysDataLoader: MultipleKeysDataLoader<K, V>): DataLoader<K, V> =
+    private fun <K, V> batchDataLoaderMappedMultiple(
+        coroutineDispatcher: ExecutorCoroutineDispatcher,
+        keysDataLoader: MultipleKeysDataLoader<K, V>
+    ): DataLoader<K, V> =
         DataLoader.newMappedDataLoaderWithTry(
             { keys: Set<K>, env: BatchLoaderEnvironment ->
-                GlobalScope.async {
+                GlobalScope.async(coroutineDispatcher) {
                     keysDataLoader.getByKeys(keys, env.keyContexts.entries.first().value as GoboGraphQLContext)
                 }.asCompletableFuture()
             },
@@ -67,21 +74,21 @@ class DataLoaderConfiguration(
     /**
      * Use this if you have a service that loads a single id. Will make requests in parallel
      */
-    private fun <K, V> batchDataLoaderMappedSingle(keyDataLoader: KeyDataLoader<K, V>): DataLoader<K, V> =
+    private fun <K, V> batchDataLoaderMappedSingle(
+        coroutineDispatcher: ExecutorCoroutineDispatcher,
+        keyDataLoader: KeyDataLoader<K, V>
+    ): DataLoader<K, V> =
         DataLoader.newMappedDataLoaderWithTry(
             { keys: Set<K>, env: BatchLoaderEnvironment ->
-                GlobalScope.async {
-                    val deferred: List<Deferred<Pair<K, Try<V>>>> = keys.map { key ->
-                        async {
-                            key to try {
-                                val ctx = env.keyContexts[key] as GoboGraphQLContext
-                                Try.succeeded(keyDataLoader.getByKey(key, ctx))
-                            } catch (e: Exception) {
-                                Try.failed<V>(e)
-                            }
+                GlobalScope.async(coroutineDispatcher) {
+                    keys.map { key ->
+                        key to try {
+                            val ctx = env.keyContexts[key] as GoboGraphQLContext
+                            Try.succeeded(keyDataLoader.getByKey(key, ctx))
+                        } catch (e: Exception) {
+                            Try.failed<V>(e)
                         }
-                    }
-                    deferred.awaitAll().toMap()
+                    }.toMap()
                 }.asCompletableFuture()
             },
             DataLoaderOptions.newOptions().setCachingEnabled(false)
