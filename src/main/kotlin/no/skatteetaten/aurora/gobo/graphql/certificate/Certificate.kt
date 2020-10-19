eb8d5c408a98fe4e65bec1e6704bce658de57f39
Jarle Hansen <jarle.hansen@skatteetaten.no>
2020-10-19 13:37:27 +0200

    added cursor to CertificateEdge

--- src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/certificate/Certificate.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/certificate/Certificate.kt
@@ -2,7 +2,11 @@ package no.skatteetaten.aurora.gobo.graphql.certificate
 
 import no.skatteetaten.aurora.gobo.graphql.GoboPageInfo
 import no.skatteetaten.aurora.gobo.graphql.application.Certificate
+import org.springframework.util.Base64Utils
 
-data class CertificateEdge(val node: Certificate)
+data class CertificateEdge(val node: Certificate) {
+    val cursor: String
+        get() = Base64Utils.encodeToString(node.id.toByteArray())
+}
 
 data class CertificatesConnection(val edges: List<CertificateEdge>, val pageInfo: GoboPageInfo?, val totalCount: Int = edges.size)
