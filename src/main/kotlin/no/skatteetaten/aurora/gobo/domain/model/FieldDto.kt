6877b1d51d7c726f5cbcebba7f3edb9321b6cf78
m78879 <jorgen.bugge@skatteetaten.no>
2020-11-12 08:25:09 +0100

    AOS-5057: Laget FieldClientDto

--- src/main/kotlin/no/skatteetaten/aurora/gobo/domain/model/FieldDto.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/domain/model/FieldDto.kt
@@ -4,3 +4,10 @@ data class FieldDto(
     val name: String,
     var count: Int? = null
 )
+
+
+//data class FieldClientDto(
+//        val fieldName: String,
+//        val name: String,
+//        var count: Int? = null
+//)
