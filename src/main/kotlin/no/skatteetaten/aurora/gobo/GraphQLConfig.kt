2606b691156fa7035951e0e39bd2b370b15b479b
Jarle Hansen <jarle.hansen@skatteetaten.no>
2020-11-18 15:17:51 +0100

    override the playgroundRoute to enable a custom html file

--- src/main/kotlin/no/skatteetaten/aurora/gobo/GraphQLConfig.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/GraphQLConfig.kt
@@ -1,20 +1,42 @@
 package no.skatteetaten.aurora.gobo
 
 import com.expediagroup.graphql.hooks.SchemaGeneratorHooks
+import com.expediagroup.graphql.spring.GraphQLConfigurationProperties
 import com.fasterxml.jackson.databind.JsonNode
 import graphql.schema.GraphQLScalarType
 import graphql.schema.GraphQLType
 import no.skatteetaten.aurora.gobo.graphql.scalars.InstantScalar
 import no.skatteetaten.aurora.gobo.graphql.scalars.JsonNodeScalar
 import no.skatteetaten.aurora.gobo.graphql.scalars.UrlScalar
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.Resource
+import org.springframework.web.reactive.function.server.bodyValueAndAwait
+import org.springframework.web.reactive.function.server.coRouter
+import org.springframework.web.reactive.function.server.html
 import java.net.URL
 import java.time.Instant
 import kotlin.reflect.KType
 
 @Configuration
-class GraphQLConfig {
+class GraphQLConfig(
+    private val config: GraphQLConfigurationProperties,
+    @Value("classpath:/playground/gobo-graphql-playground.html") private val playgroundHtml: Resource
+) {
+    private val body = playgroundHtml.inputStream.bufferedReader().use { reader ->
+        reader.readText()
+            .replace("\${graphQLEndpoint}", config.endpoint)
+            .replace("\${subscriptionsEndpoint}", config.subscriptions.endpoint)
+    }
+
+    @Bean
+    fun playgroundRouteGobo() = coRouter {
+        GET(config.playground.endpoint) {
+            ok().html().bodyValueAndAwait(body)
+        }
+    }
+
     @Bean
     fun hooks() = GoboSchemaGeneratorHooks()
 }
