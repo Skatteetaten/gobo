dcdcd9314d949ae1f40a4465ca5b4138a7c389ec
Jarle Hansen <jarle.hansen@skatteetaten.no>
2020-11-25 15:16:17 +0100

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,24 +23,24 @@ aurora {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
 
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.0.2.RELEASE")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.0")
     implementation("org.apache.commons:commons-text:1.9")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("com.expediagroup:graphql-kotlin-spring-server:3.6.6")
+    implementation("com.expediagroup:graphql-kotlin-spring-server:3.6.7")
 
     implementation("uk.q3c.rest:hal-kotlin:0.5.4.0.db32476") {
         exclude(group = "com.google.guava", module = "guava")
     }
-    implementation("io.fabric8:openshift-client:4.12.0")
+    implementation("io.fabric8:openshift-client:4.13.0")
     implementation("com.github.fge:json-patch:1.13")
     implementation("com.jayway.jsonpath:json-path:2.4.0")
-    implementation("io.projectreactor.addons:reactor-extra:3.3.3.RELEASE")
+    implementation("io.projectreactor.addons:reactor-extra:3.4.0")
     implementation("no.skatteetaten.aurora.kubernetes:kubernetes-reactor-coroutines-client:1.3.1")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("io.mockk:mockk:1.10.2")
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.22")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.23")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:1.1.5")
     testImplementation("com.ninja-squad:springmockk:2.0.3")
@@ -48,7 +48,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.1")
 }
 
-val downloadPlaygroundfiles = task<de.undercouch.gradle.tasks.download.Download>("download-playground") {
+task<de.undercouch.gradle.tasks.download.Download>("download-playground") {
     val baseUrl = "https://cdn.jsdelivr.net/npm/graphql-playground-react/build"
     src(
         listOf(
