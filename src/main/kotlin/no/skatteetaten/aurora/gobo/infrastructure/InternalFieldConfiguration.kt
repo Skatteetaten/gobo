e720c35af43942c29fd4876d23cb006de65caead
m78879 <jorgen.bugge@skatteetaten.no>
2020-11-30 09:10:55 +0100

    AOS-5057: La til EnableScheduling

--- src/main/kotlin/no/skatteetaten/aurora/gobo/infrastructure/InternalFieldConfiguration.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/infrastructure/InternalFieldConfiguration.kt
@@ -5,10 +5,12 @@ import no.skatteetaten.aurora.gobo.infrastructure.entity.FieldEnity
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @Configuration
 @EnableJpaRepositories(basePackageClasses = arrayOf(FieldRepository::class))
 @EntityScan(basePackageClasses = arrayOf(FieldEnity::class))
 @EnableTransactionManagement
+@EnableScheduling
 internal class InternalFieldConfiguration
