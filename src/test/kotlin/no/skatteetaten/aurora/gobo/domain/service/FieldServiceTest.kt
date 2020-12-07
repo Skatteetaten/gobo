d0fffac958717b24396aff3263ba02df1423bcd3
Jarle Hansen <jarle@jarlehansen.net>
2020-12-07 10:48:07 +0100

    added test for failing addField (#194)

--- src/test/kotlin/no/skatteetaten/aurora/gobo/domain/service/FieldServiceTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/gobo/domain/service/FieldServiceTest.kt
@@ -3,7 +3,8 @@ package no.skatteetaten.aurora.gobo.domain.service
 import assertk.assertThat
 import assertk.assertions.hasSize
 import assertk.assertions.isEqualTo
-import assertk.assertions.isNotNull
+import assertk.assertions.isFailure
+import assertk.assertions.isInstanceOf
 import no.skatteetaten.aurora.gobo.domain.FieldService
 import no.skatteetaten.aurora.gobo.domain.model.FieldClientDto
 import no.skatteetaten.aurora.gobo.domain.model.FieldDto
@@ -13,6 +14,7 @@ import no.skatteetaten.aurora.gobo.infrastructure.repository.FieldRepository
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.dao.DuplicateKeyException
 import org.springframework.test.context.ContextConfiguration
 
 @ContextConfiguration(classes = [FieldClientRepository::class, FieldServiceImpl::class, FieldRepository::class])
@@ -22,66 +24,56 @@ class FieldServiceTest {
     @Autowired
     private lateinit var service: FieldService
 
-    @Test
-    fun `'addField' should return created entity`() {
-        service.addField(
-            FieldDto(
-                name = "gobo.usage.usedFields.name",
-                count = 10,
-                clients = listOf(FieldClientDto("donald", 5), FieldClientDto("duck", 5))
-            )
-        )
+    private val field1 = FieldDto(
+        name = "gobo.usage.usedFields",
+        count = 10,
+        clients = listOf(FieldClientDto("donald", 5), FieldClientDto("duck", 5))
+    )
 
-        val field = service.getFieldWithName("gobo.usage.usedFields.name")!!
-        assertThat(field.name).isEqualTo("gobo.usage.usedFields.name")
+    private val field2 = FieldDto(
+        name = "gobo.usage.usedFields.name",
+        count = 40,
+        clients = listOf(FieldClientDto("donald", 40))
+    )
+
+    @Test
+    fun `Save new field`() {
+        service.addField(field1)
+
+        val field = service.getFieldWithName(field1.name)!!
+        assertThat(field.name).isEqualTo("gobo.usage.usedFields")
         assertThat(field.count).isEqualTo(10)
         assertThat(field.clients).hasSize(2)
         assertThat(field.clients[0].count).isEqualTo(5)
     }
 
     @Test
-    fun `'insertOrUpdateField' first addField and when update field should return created entity`() {
-        service.addField(
-            FieldDto(
-                name = "gobo.usage.usedFields.name",
-                count = 10,
-                clients = listOf(FieldClientDto("donald", 5), FieldClientDto("duck", 5))
-            )
-        )
-        val insertField = service.getFieldWithName("gobo.usage.usedFields.name")!!
-        assertThat(insertField.name).isEqualTo("gobo.usage.usedFields.name")
-        assertThat(insertField.count).isEqualTo(10)
-
-        val updatedField = insertField.copy(count = 12, clients = listOf(FieldClientDto("donald", 12)))
+    fun `Update existing field with new count and client`() {
+        service.addField(field1)
+        val updatedField = field1.copy(count = 12, clients = listOf(FieldClientDto("donald", 12)))
         service.insertOrUpdateField(updatedField)
-        val persistedField = service.getFieldWithName(updatedField.name)
-        assertThat(persistedField?.name).isNotNull().isEqualTo("gobo.usage.usedFields.name")
-        assertThat(persistedField?.count).isNotNull().isEqualTo(22)
+
+        val persistedField = service.getFieldWithName(updatedField.name)!!
+        assertThat(persistedField.name).isEqualTo("gobo.usage.usedFields")
+        assertThat(persistedField.count).isEqualTo(22)
     }
 
     @Test
-    fun `'getAllFields' should return list of created entity`() {
-        val tmpField1 = FieldDto(
-            name = "gobo.usage.usedFields",
-            count = 42,
-            clients =
-                listOf(FieldClientDto("donald", 42))
-        )
-        service.insertOrUpdateField(tmpField1)
+    fun `Get all persisted fields and clients`() {
+        service.insertOrUpdateField(field1)
+        service.insertOrUpdateField(field2)
 
-        val tmpField2 = FieldDto(
-            name = "gobo.usage.usedFields.name",
-            count = 40,
-            clients =
-                listOf(FieldClientDto("donald", 40))
-        )
-        service.insertOrUpdateField(tmpField2)
+        val fields = service.getAllFields()
+        assertThat(fields.size).isEqualTo(2)
+        assertThat(fields[0]).isEqualTo(field1)
+        assertThat(fields[1]).isEqualTo(field2)
+        assertThat(fields[0].clients).hasSize(2)
+        assertThat(fields[1].clients).hasSize(1)
+    }
 
-        val listOfFields = service.getAllFields()
-        assertThat(listOfFields.size).isEqualTo(2)
-        assertThat(listOfFields[0]).isEqualTo(tmpField1)
-        assertThat(listOfFields[1]).isEqualTo(tmpField2)
-        assertThat(listOfFields[0].clients).hasSize(1)
-        assertThat(listOfFields[1].clients).hasSize(1)
+    @Test
+    fun `Throw exception when trying to add same field twice`() {
+        service.addField(field1)
+        assertThat { service.addField(field1) }.isFailure().isInstanceOf(DuplicateKeyException::class)
     }
 }
