a3e33d30516103f48cc6466bb034beaccf007b88
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2020-12-23 13:03:32 +0100

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "1.4.21"
     id("no.skatteetaten.gradle.aurora") version "4.1.1"
-    id("org.flywaydb.flyway") version "7.1.1"
+    id("org.flywaydb.flyway") version "7.3.2"
 }
 
 aurora {
@@ -24,10 +24,10 @@ aurora {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
 
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.0")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.1")
     implementation("org.apache.commons:commons-text:1.9")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("com.expediagroup:graphql-kotlin-spring-server:3.6.8")
+    implementation("com.expediagroup:graphql-kotlin-spring-server:3.7.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("io.github.microutils:kotlin-logging-jvm:2.0.4")
 
@@ -43,16 +43,16 @@ dependencies {
     }
     implementation("io.fabric8:openshift-client:4.13.0")
     implementation("com.github.fge:json-patch:1.13")
-    implementation("com.jayway.jsonpath:json-path:2.4.0")
-    implementation("io.projectreactor.addons:reactor-extra:3.4.0")
+    implementation("com.jayway.jsonpath:json-path:2.5.0")
+    implementation("io.projectreactor.addons:reactor-extra:3.4.1")
     implementation("no.skatteetaten.aurora.kubernetes:kubernetes-reactor-coroutines-client:1.3.4")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("io.mockk:mockk:1.10.2")
+    testImplementation("io.mockk:mockk:1.10.3")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.23")
     testImplementation("io.projectreactor:reactor-test")
-    testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:1.1.5")
+    testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:1.1.6")
     testImplementation("com.ninja-squad:springmockk:2.0.3")
     testImplementation("org.junit-pioneer:junit-pioneer:1.1.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.1")
