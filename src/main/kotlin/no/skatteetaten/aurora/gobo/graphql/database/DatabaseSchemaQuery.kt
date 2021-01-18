cd08e7838f32398716237c20a846690e100a221c
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2021-01-18 12:13:40 +0100

    support the old databaseSchema query until aurora konsoll is released

--- src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/database/DatabaseSchemaQuery.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/database/DatabaseSchemaQuery.kt
@@ -27,7 +27,18 @@ class DatabaseSchemaQuery(val databaseService: DatabaseService) : Query {
         return DatabaseSchema.create(databaseSchema, Affiliation(databaseSchema.affiliation))
     }
 
-    suspend fun databaseSchemas(
+    // TODO Remove after AOS-5305 is released
+    suspend fun databaseSchemas(affiliations: List<String>, dfe: DataFetchingEnvironment): List<DatabaseSchema> {
+        dfe.checkValidUserToken()
+        return affiliations
+            .flatMap { affiliation ->
+                databaseService.getDatabaseSchemas(affiliation)
+                    .map { DatabaseSchema.create(it, Affiliation(affiliation)) }
+            }
+    }
+
+    // TODO Rename to databaseSchemas after AOS-5305 is released
+    suspend fun databaseSchemasPagination(
         affiliations: List<String>,
         first: Int,
         after: String?,
