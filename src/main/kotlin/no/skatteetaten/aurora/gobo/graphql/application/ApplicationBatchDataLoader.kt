26bb932246ef34104b0b5be4c6300812667c12bf
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2021-01-29 07:35:57 +0100

    loop over each affiliation and group the applications found

--- src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/application/ApplicationBatchDataLoader.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/application/ApplicationBatchDataLoader.kt
@@ -5,22 +5,21 @@ import no.skatteetaten.aurora.gobo.graphql.GoboGraphQLContext
 import no.skatteetaten.aurora.gobo.graphql.applicationdeployment.ApplicationDeployment
 import no.skatteetaten.aurora.gobo.integration.mokey.ApplicationService
 import org.springframework.stereotype.Component
-import org.springframework.util.LinkedMultiValueMap
 
 @Component
 class ApplicationBatchDataLoader(val applicationService: ApplicationService) :
     KeysBatchDataLoader<String, List<Application>> {
 
-    override suspend fun getByKeys(keys: Set<String>, context: GoboGraphQLContext): Map<String, List<Application>> {
-        val applications = applicationService.getApplications(keys.toList())
-        val results = LinkedMultiValueMap<String, Application>()
-        applications.forEach {
-            results.add(
-                it.applicationDeployments.first().affiliation,
+    override suspend fun getByKeys(
+        affiliations: Set<String>,
+        context: GoboGraphQLContext
+    ): Map<String, List<Application>> {
+        val applications = applicationService.getApplications(affiliations.toList())
+        return affiliations.map { affiliation ->
+            val apps = applications.filter { it.applicationDeployments.first().affiliation == affiliation }.map {
                 Application(it.identifier, it.name, it.applicationDeployments.map { ApplicationDeployment.create(it) })
-            )
-        }
-
-        return results
+            }
+            affiliation to apps
+        }.toMap()
     }
 }
