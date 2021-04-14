30903a7d7e1237589e6e32066722e3137e248d0c
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2021-04-14 09:33:04 +0200

    refactored the creation of environment resources

--- src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/environment/EnvironmentQuery.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/environment/EnvironmentQuery.kt
@@ -4,6 +4,8 @@ import com.expediagroup.graphql.spring.operations.Query
 import graphql.schema.DataFetchingEnvironment
 import no.skatteetaten.aurora.gobo.graphql.token
 import no.skatteetaten.aurora.gobo.integration.boober.EnvironmentService
+import no.skatteetaten.aurora.gobo.integration.boober.MultiAffiliationEnvironment
+import no.skatteetaten.aurora.gobo.integration.mokey.ApplicationDeploymentResource
 import no.skatteetaten.aurora.gobo.integration.mokey.ApplicationService
 import no.skatteetaten.aurora.gobo.security.checkValidUserToken
 import org.springframework.stereotype.Component
@@ -28,16 +30,29 @@ class EnvironmentQuery(
                 .filter { it.environment == name }
                 .groupBy { it.affiliation }
                 .map {
-                    it.value.map { ad ->
-                        environments
-                            .filter { env -> env.affiliation == ad.affiliation }
-                            .flatMap { env -> env.deploymentRefs }
-                            .find { ref -> ref.environment == ad.environment && ref.application == ad.name }
-                            .let { deploymentRef -> EnvironmentApplication.create(ad, deploymentRef) }
-                    }.let { apps -> EnvironmentAffiliation(it.key, apps) }
+                    buildResources(
+                        affiliation = it.key,
+                        applicationDeployments = it.value,
+                        environments = environments
+                    )
                 }
+
+            // TODO filtrer på statusene fra Phil
+            // 1) hvis status fra Phil er failed, bruk denne 2) hvis status fra Phil er success, bruk status fra ApplicationDeployment (Mokey)
 
             Environment(name, affiliations)
         }
     }
+
+    private fun buildResources(
+        affiliation: String,
+        applicationDeployments: List<ApplicationDeploymentResource>,
+        environments: List<MultiAffiliationEnvironment>
+    ) = applicationDeployments.map { ad ->
+        environments
+            .filter { env -> env.affiliation == ad.affiliation }
+            .flatMap { env -> env.deploymentRefs }
+            .find { ref -> ref.environment == ad.environment && ref.application == ad.name }
+            .let { deploymentRef -> EnvironmentApplication.create(ad, deploymentRef) }
+    }.let { apps -> EnvironmentAffiliation(affiliation, apps) }
 }
