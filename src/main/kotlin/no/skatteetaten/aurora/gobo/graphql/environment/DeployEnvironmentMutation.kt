42fcf8f40f40160a281ee926949a7dd1049c2dba
Rune Offerdal <Rune.Offerdal@skatteetaten.no>
2021-05-26 16:26:44 +0200

    AOS-5552 fixing bugs

--- src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/environment/DeployEnvironmentMutation.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/environment/DeployEnvironmentMutation.kt
@@ -1,17 +1,15 @@
-package no.skatteetaten.aurora.gobo.graphql.applicationdeployment
+package no.skatteetaten.aurora.gobo.graphql.environment
 
 import com.expediagroup.graphql.server.operations.Mutation
 import graphql.schema.DataFetchingEnvironment
 import no.skatteetaten.aurora.gobo.graphql.token
 import no.skatteetaten.aurora.gobo.integration.phil.PhilService
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 
 @Component
 class DeploymentEnvironmentMutation(
-    @Qualifier("philServiceReactive") private val philService: PhilService,
+    private val philService: PhilService,
 ) : Mutation {
-
     suspend fun deployEnvironment(input: DeploymentEnvironmentInput, dfe: DataFetchingEnvironment): DeploymentEnvironmentResponse {
         val philDeploymentResult = philService.deployEnvironment(input.environment, dfe.token())
         return DeploymentEnvironmentResponse(philDeploymentResult.success)
