f0a951a26065ae52c8b7018c0296dbe29198fff0
Engebretsen, Tomas <Tomas.Engebretsen@skatteetaten.no>
2021-09-02 16:25:47 +0200

    Remove deprecated parameter

--- src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/affiliation/AffiliationQuery.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/affiliation/AffiliationQuery.kt
@@ -3,7 +3,6 @@ package no.skatteetaten.aurora.gobo.graphql.affiliation
 import com.expediagroup.graphql.server.operations.Query
 import graphql.schema.DataFetchingEnvironment
 import mu.KotlinLogging
-import no.skatteetaten.aurora.gobo.graphql.GoboGraphQLContext
 import no.skatteetaten.aurora.gobo.graphql.token
 import no.skatteetaten.aurora.gobo.service.AffiliationService
 import org.springframework.stereotype.Component
@@ -14,21 +13,11 @@ private val logger = KotlinLogging.logger { }
 class AffiliationQuery(val affiliationService: AffiliationService) : Query {
 
     suspend fun affiliations(
-        name: String? = null, // TODO: This query variable is deprecated, replaceWith names
         names: List<String>? = null,
         checkForVisibility: Boolean? = null,
         includeUndeployed: Boolean? = null,
         dfe: DataFetchingEnvironment
     ): AffiliationsConnection {
-
-        // TODO: This block of code below should be removed
-        name?.let {
-            val context = dfe.getContext<GoboGraphQLContext>()
-            context?.klientid()?.let { client ->
-                logger.info("Client $client is using the deprecated name query variable in the affiliation query: ${context.query}")
-            }
-            return AffiliationsConnection(listOf(AffiliationEdge(Affiliation(name))))
-        }
 
         val affiliationNames = if (names.isNullOrEmpty()) {
             getAffiliations(checkForVisibility ?: false, includeUndeployed ?: false, dfe)
