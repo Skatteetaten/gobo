dc89e2de19cedc2dd7cc175aed888f7b566e5262
Jarle Hansen <jarle@jarlehansen.net>
2021-12-31 08:12:29 +0100

    updated to new stubs (#264)
    * updated to new stubs
    
    * bump dependencies
    
    * removed unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.5.31"
-    id("no.skatteetaten.gradle.aurora") version "4.4.5"
-    id("io.gatling.gradle") version "3.7.2"
+    kotlin("jvm") version "1.6.10"
+    id("no.skatteetaten.gradle.aurora") version "4.4.9"
+    id("io.gatling.gradle") version "3.7.3"
 }
 
 aurora {
@@ -15,12 +15,12 @@ aurora {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
 
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.5")
     implementation("org.apache.commons:commons-text:1.9")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("com.expediagroup:graphql-kotlin-spring-server:5.3.0")
+    implementation("com.expediagroup:graphql-kotlin-spring-server:5.3.1")
     implementation("com.graphql-java:graphql-java-extended-scalars:17.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.apache.commons:commons-collections4:4.4")
@@ -33,9 +33,8 @@ dependencies {
         exclude(group = "com.google.guava", module = "guava")
     }
 
-    implementation("com.github.fge:json-patch:1.13")
     implementation("com.jayway.jsonpath:json-path:2.6.0")
-    implementation("io.projectreactor.addons:reactor-extra:3.4.5")
+    implementation("io.projectreactor.addons:reactor-extra:3.4.6")
     implementation("no.skatteetaten.aurora.kubernetes:kubernetes-reactor-coroutines-client:1.3.21")
     implementation("no.skatteetaten.aurora.springboot:aurora-spring-security-starter:1.6.3")
 
@@ -47,7 +46,7 @@ dependencies {
     testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.2.0") {
         exclude(group = "no.skatteetaten.aurora.springboot", module = "aurora-spring-boot-mvc-starter")
     }
-    testImplementation("com.ninja-squad:springmockk:3.0.1")
+    testImplementation("com.ninja-squad:springmockk:3.1.0")
     testImplementation("org.junit-pioneer:junit-pioneer:1.5.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
 }
