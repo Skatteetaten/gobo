d87877e6151536042b76d73cb182d408a0070521
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-04-07 12:58:23 +0200

    added unfinished queries to liveness check

--- src/main/kotlin/no/skatteetaten/aurora/gobo/GoboLiveness.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/GoboLiveness.kt
@@ -3,6 +3,7 @@ package no.skatteetaten.aurora.gobo
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Tag
 import mu.KotlinLogging
+import no.skatteetaten.aurora.gobo.graphql.QueryReporter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint
 import org.springframework.http.ResponseEntity
@@ -16,8 +17,10 @@ data class ConnectionPool(val totalConnections: Double, val pendingConnections: 
 @Component
 class GoboLiveness(
     private val meterRegistry: MeterRegistry,
+    private val queryReporter: QueryReporter,
     @Value("\${gobo.liveness.maxTotalConnections:14}") private val maxTotalConnections: Int,
-    @Value("\${gobo.liveness.maxPendingConnections:2}") private val maxPendingConnections: Int
+    @Value("\${gobo.liveness.maxPendingConnections:2}") private val maxPendingConnections: Int,
+    @Value("\${gobo.liveness.maxUnfinishedQueries:2}") private val maxUnfinishedQueries: Int,
 ) {
 
     val nettyTotalConnections = "reactor.netty.connection.provider.total.connections"
@@ -38,6 +41,19 @@ class GoboLiveness(
                 }
             }
         }
+
+    fun getUnfinishedQueries() =
+        queryReporter
+            .unfinishedQueries()
+            .also {
+                if (it.size > maxUnfinishedQueries) {
+                    logger.warn { "Liveness check failed with ${it.size} number of unfinished queries" }
+                }
+
+                it.forEach { query ->
+                    logger.warn { """Unfinished query, Korrelasjonsid=${query.korrelasjonsid} Klientid="${query.klientid}" started="${query.started}" name=${query.name} query="${query.query}" """ }
+                }
+            }
 }
 
 @Component
@@ -45,9 +61,10 @@ class GoboLiveness(
 class GoboLivenessController(private val goboLiveness: GoboLiveness) {
 
     @GetMapping
-    fun liveness(): ResponseEntity<Map<String, List<ConnectionPool>>> {
+    fun liveness(): ResponseEntity<Map<String, List<*>>> {
         logger.debug("Liveness check called")
         val connectionPools = goboLiveness.getConnectionPools()
-        return ResponseEntity.ok(mapOf("connectionPools" to connectionPools))
+        val unfinishedQueries = goboLiveness.getUnfinishedQueries()
+        return ResponseEntity.ok(mapOf("connectionPools" to connectionPools, "unfinishedQueries" to unfinishedQueries))
     }
 }
