4f1d291dbf659f1d5969d1b127cbaab262fa3b1e
Kristoffer Moberg Christensen <kristoffermoberg.christensen@skatteetaten.no>
2022-05-12 09:32:12 +0200

    feat: get storagegridObjectAreaResource from mokey

---
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/integration/mokey/StoragegridObjectAreasService.kt
@@ -0,0 +1,34 @@
+package no.skatteetaten.aurora.gobo.integration.mokey
+
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import no.skatteetaten.aurora.gobo.ServiceTypes
+import no.skatteetaten.aurora.gobo.TargetService
+import no.skatteetaten.aurora.gobo.integration.awaitWithRetry
+import uk.q3c.rest.hal.HalResource
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class StoragegridObjectAreaResource(
+    val name: String,
+    val namespace: String,
+    val creationTimestamp: String,
+    val objectArea: String,
+    val bucketName: String,
+    val message: String,
+    val reason: String,
+    val success: Boolean,
+) : HalResource()
+
+@Service
+class StoragegridObjectAreasService(@TargetService(ServiceTypes.MOKEY) val webClient: WebClient) {
+    suspend fun getObjectAreas(affiliation: String, token: String): List<StoragegridObjectAreaResource> {
+        return webClient.get()
+            .uri("/api/auth/storagegridobjectarea?affiliation={affiliation}", affiliation)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+            .retrieve()
+            .handleHttpStatusErrors(affiliation)
+            .awaitWithRetry()
+    }
+}
