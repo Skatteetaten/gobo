7aaf95aa6744e61bdeab45f686b6940f64ab4611
Frode Mathiesen <Frode.Mathiesen@skatteetaten.no>
2022-08-04 12:30:16 +0200

    implement cname api supporting multiple types

--- src/main/kotlin/no/skatteetaten/aurora/gobo/integration/spotless/SpotlessCnameService.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/integration/spotless/SpotlessCnameService.kt
@@ -4,7 +4,7 @@ import no.skatteetaten.aurora.gobo.RequiresSpotless
 import no.skatteetaten.aurora.gobo.ServiceTypes
 import no.skatteetaten.aurora.gobo.TargetService
 import no.skatteetaten.aurora.gobo.graphql.IntegrationDisabledException
-import no.skatteetaten.aurora.gobo.graphql.cname.CnameContent
+import no.skatteetaten.aurora.gobo.graphql.cname.CnameAzure
 import no.skatteetaten.aurora.gobo.integration.awaitWithRetry
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 
 interface SpotlessCnameService {
-    suspend fun getCnameContent(): List<CnameContent> = integrationDisabled()
+    suspend fun getCnameContent(): List<CnameAzure> = integrationDisabled()
 
     private fun integrationDisabled(): Nothing =
         throw IntegrationDisabledException("Spotless integration is disabled for this environment")
@@ -21,7 +21,7 @@ interface SpotlessCnameService {
 @Service
 @ConditionalOnBean(RequiresSpotless::class)
 class SpotlessCnameServiceReactive(@TargetService(ServiceTypes.SPOTLESS) val webClient: WebClient) : SpotlessCnameService {
-    override suspend fun getCnameContent(): List<CnameContent> =
+    override suspend fun getCnameContent(): List<CnameAzure> =
         webClient
             .get()
             .uri("/api/v1/cname")
