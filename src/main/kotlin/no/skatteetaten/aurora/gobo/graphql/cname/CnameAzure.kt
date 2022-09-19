fd7575571db1052e7877f86435b15d49048371b8
Martin Rødland <martin.rodland@skatteetaten.no>
2022-09-19 15:48:30 +0200

    added name to cname azure (#322)

--- src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/cname/CnameAzure.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gobo/graphql/cname/CnameAzure.kt
@@ -1,6 +1,7 @@
 package no.skatteetaten.aurora.gobo.graphql.cname
 
 data class CnameAzure(
+    val name: String,
     val canonicalName: String,
     val ttlInSeconds: Int,
     val namespace: String,
